946c560da2682d0da8585b99b025fdf91f3e81ca
Duc Huy <nd.huyhuy2504@gmail.com>
2025-07-30 11:12:29 +0700

    Reapply "first project"
    This reverts commit 508f3cc67f85affb549e78081cdc190733d7082c.

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header
+            style={{
+                backgroundColor: "lightblue",
+                padding: "1rem"
+            }}
+        >
+            <p>Header</p>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,5 @@
+const Profile = () => {
+    return <h1>My profile</h1>
+}
+
+export default Profile;
