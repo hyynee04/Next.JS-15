c39fff734a4d71e11353ffd559f7e0104f2dc837
Duc Huy <nd.huyhuy2504@gmail.com>
2025-07-30 14:33:31 +0700

    title Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "About Learning Next.JS"
+}
+
 const About = () => {
     return <h1>About me</h1>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,24 @@
-const ProductDetails = async ({
-    params
-}: {
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props = {
     params: Promise<{ productId: string }>
-}) => {
-    const { productId } = await params;
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iPhone ${id}`)
+        }, 100)
+    })
+    return {
+        title: `Product ${title}`
+    }
+}
+
+const ProductDetails = async ({ params }: Props) => {
+    const productId = (await params).productId;
     return <h1>Details about product {productId}</h1>
 }
 
